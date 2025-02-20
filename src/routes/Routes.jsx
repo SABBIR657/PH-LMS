c4a3a59ac9a04a0d95f5017d68c9135439e23e77
Sazzad-Hosen-Mahim <sajjadhosenmahim@gmail.com>
2025-02-20 11:16:15 +0600

    route created

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -4,6 +4,13 @@ import App from "../App";
 import NotFound from "../pages/NotFound";
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
+import CourseDetails from "../pages/CourseDetails";
+import About from "../pages/About";
+import Blog from "../pages/Blog";
+import Success from "../pages/Success";
+import Support from "../pages/Support";
+import MyClass from "../pages/MyClass";
+import Profile from "../pages/Profile";
 
 const routes = createBrowserRouter([
   {
@@ -16,12 +23,38 @@ const routes = createBrowserRouter([
         element: <Home />,
       },
       {
-        index: true,
-        path: "/login",
-        element: <SignIn /> ,
+        path: "/course-details",
+        element: <CourseDetails />,
       },
       {
-        index: true,
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/blog",
+        element: <Blog />,
+      },
+      {
+        path: "/success",
+        element: <Success />,
+      },
+      {
+        path: "/support",
+        element: <Support />,
+      },
+      {
+        path: "/my-class",
+        element: <MyClass />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
+      {
+        path: "/login",
+        element: <SignIn />,
+      },
+      {
         path: "/signup",
         element: <SignUp />,
       },
