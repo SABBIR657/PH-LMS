c643ff31e82d057c02468aaee370b4bbe089726e
SABBIR657 <mdsabbirrahman2025@gmail.com>
2025-02-20 12:30:35 +0600

    tailwind config change

---
+++ src/components/Dummy.tsx
@@ -0,0 +1,10 @@
+
+type Props = {}
+
+const Dummy = (props: Props) => {
+  return (
+    <div>Dummy from ts</div>
+  )
+}
+
+export default Dummy
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,10 @@ const {heroui} = require("@heroui/react");
 module.exports = {
   darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,jsx}",
-    "./components/**/*.{js,jsx}",
-    "./app/**/*.{js,jsx}",
-    "./src/**/*.{js,jsx}",
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
