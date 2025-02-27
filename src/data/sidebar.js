179c6f421ba771dd2eaec415ce89a30da5995023
Softvence <softvence@Softvences-Mac-mini.local>
2025-02-27 12:53:07 +0600

    side bar and all list show done

---
+++ src/data/sidebar.js
@@ -0,0 +1,86 @@
+export const sidebarUrlList = [
+  {
+    parentLabel: "Course",
+    children: [
+      {
+        label: "Course List",
+        url: "/admin/course",
+      },
+      {
+        label: "Course Create",
+        url: "/admin/course-create",
+      },
+      {
+        label: "Course Details",
+        url: "/admin/course-details",
+      },
+      {
+        label: "Course Update",
+        url: "/admin/course-update",
+      },
+    ],
+  },
+  {
+    parentLabel: "Milestone",
+    children: [
+      {
+        label: "Milestone List",
+        url: "/admin/milestone",
+      },
+      {
+        label: "Milestone Create",
+        url: "/admin/milestone-create",
+      },
+      {
+        label: "Milestone Details",
+        url: "/admin/milestone-details",
+      },
+      {
+        label: "Milestone Update",
+        url: "/admin/milestone-update",
+      },
+    ],
+  },
+  {
+    parentLabel: "Module",
+    children: [
+      {
+        label: "Module List",
+        url: "/admin/module",
+      },
+      {
+        label: "Module Create",
+        url: "/admin/module-create",
+      },
+      {
+        label: "Module Details",
+        url: "/admin/module-details",
+      },
+      {
+        label: "Module Update",
+        url: "/admin/module-update",
+      },
+    ],
+  },
+  {
+    parentLabel: "Video",
+    children: [
+      {
+        label: "Video List",
+        url: "/admin/video",
+      },
+      {
+        label: "Video Create",
+        url: "/admin/video-create",
+      },
+      {
+        label: "Video Details",
+        url: "/admin/video-details",
+      },
+      {
+        label: "Video Update",
+        url: "/admin/video-update",
+      },
+    ],
+  },
+];
--- src/pages/Admin/course/CourseList.jsx
+++ src/pages/Admin/course/CourseList.jsx
@@ -1,9 +1,36 @@
 import React from "react";
+import useFetchQuery from "../../../hooks/shared/useFetch";
+import CourseListTable from "../../../components/Admin/course/CourseListTable";
 
 const CourseList = () => {
+  const { data, isLoading, isSuccess, refetch } = useFetchQuery(
+    "/course/all-courses"
+  );
+
+  const onDelete = (id) => {
+    console.log(id);
+  };
+  const onView = (id) => {
+    console.log(id);
+  };
+  const onEdit = (id) => {
+    console.log(id);
+  };
+
+  console.log(data);
   return (
     <div>
-      <h1>Course List</h1>
+      <h1 className="text-center text-xl font-semibold text-grays-600 py-5 shadow-sm rounded-lg bg-gray-100 mb-4">
+        Course List
+      </h1>
+      <CourseListTable
+        courses={data?.data}
+        onDelete={onDelete}
+        onView={onView}
+        onEdit={onEdit}
+        isLoading={isLoading}
+        createLink="/admin/create-course"
+      />
     </div>
   );
 };
