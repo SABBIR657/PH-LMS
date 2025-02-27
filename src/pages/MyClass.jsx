55b10d8c2083747c05e3008c1b677216025c2760
Sazzad-Hosen-Mahim <sajjadhosenmahim@gmail.com>
2025-02-27 16:15:34 +0600

    class page fixed

--- src/pages/MyClass.jsx
+++ src/pages/MyClass.jsx
@@ -82,9 +82,7 @@ const MyClass = () => {
                             <div className="mt-4 flex gap-4">
                               <button
                                 className="px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
-                                onClick={() =>
-                                  navigate(`/new-class/${course._id}`)
-                                }
+                                onClick={() => navigate(`/class/${course._id}`)}
                               >
                                 Continue Course
                               </button>
