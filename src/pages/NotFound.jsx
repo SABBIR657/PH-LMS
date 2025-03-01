bd79be52112ddc0ee355fae9b2e5eed406195ea0
Softvence <softvence@Softvences-Mac-mini.local>
2025-03-01 17:09:48 +0600

    updated

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,16 +1,19 @@
-import { Divider } from '@heroui/react';
-import { useRouteError } from 'react-router-dom';
+import { Divider } from "@heroui/react";
+import { useEffect } from "react";
+import { useRouteError } from "react-router-dom";
 
 export default function NotFound() {
   const error = useRouteError();
   console.log(error);
   return (
-    <div className="h-screen flex justify-center items-center">
-      <p className="font-bold text-2xl"> 404</p>
-      <div className="h-12 mx-6">
-        <Divider orientation="vertical" />
-      </div>
-      <p>This page could not be found.</p>
+    <div>
+      <div className="h-screen flex justify-center items-center">
+        <p className="font-bold text-2xl"> 404</p>
+        <div className="h-12 mx-6">
+          <Divider orientation="vertical" />
+        </div>
+        <p>This page could not be found.</p>
+      </div>{" "}
     </div>
   );
 }
