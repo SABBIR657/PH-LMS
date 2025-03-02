9856d69ca89f93266bdf1ce9c2b1b89ea8df2397
EngrArfin <arfin.cse.green.edu.bd@gmail.com>
2025-03-02 15:58:49 +0600

    all project

---
+++ src/components/Header/AllProjects.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+const AllProjects = () => {
+    return (
+        <div>
+            <div className="mt-32">
+                <h1 className="text-6xl font-semibold text-center mb-8 bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-indigo-600">
+                    projects you will build_
+                </h1>
+                <p className="text-lg text-gray-400  flex flex-col items-center justify-center mb-12 ">
+                    Can you imagine, you will build 45 projects in this course. At least 20 projects will be in the content. 15+ projects in the conceptual session and 10+ practice projects.                </p>
+            </div>
+            <div>
+                <div>
+                    <img src="https://web.programming-hero.com/home/_next/image?url=%2Fhome%2Fprojects%2Fportfolio.png&w=1920&q=75" alt="" />
+                    <div>
+                        <h1 className='text-4xl'>Portfolio Website</h1>
+                        <p>
+                            Build a Stunningly Simple Developer Portfolio Website (HTML & CSS Only!) This website clarifies the purpose (building a portfolio website) and emphasizes simplicity (using only HTML and CSS).
+                        </p>
+                        <Button>Learn More</Button>
+                    </div>
+                </div>
+                <div>
+                    <img src="https://web.programming-hero.com/home/_next/image?url=%2Fhome%2Fprojects%2Fportfolio.png&w=1920&q=75" alt="" />
+                    <div>
+                        <h1 className='text-4xl'>Portfolio Website</h1>
+                        <p>
+                            Build a Stunningly Simple Developer Portfolio Website (HTML & CSS Only!) This website clarifies the purpose (building a portfolio website) and emphasizes simplicity (using only HTML and CSS).
+                        </p>
+                        <Button>Learn More</Button>
+                    </div>
+                </div>
+                <div>
+                    <img src="https://web.programming-hero.com/home/_next/image?url=%2Fhome%2Fprojects%2Fportfolio.png&w=1920&q=75" alt="" />
+                    <div>
+                        <h1 className='text-4xl'>Portfolio Website</h1>
+                        <p>
+                            Build a Stunningly Simple Developer Portfolio Website (HTML & CSS Only!) This website clarifies the purpose (building a portfolio website) and emphasizes simplicity (using only HTML and CSS).
+                        </p>
+                        <Button>Learn More</Button>
+                    </div>
+                </div>
+                <div>
+                    <img src="https://web.programming-hero.com/home/_next/image?url=%2Fhome%2Fprojects%2Fportfolio.png&w=1920&q=75" alt="" />
+                    <div>
+                        <h1 className='text-4xl'>Portfolio Website</h1>
+                        <p>
+                            Build a Stunningly Simple Developer Portfolio Website (HTML & CSS Only!) This website clarifies the purpose (building a portfolio website) and emphasizes simplicity (using only HTML and CSS).
+                        </p>
+                        <Button>Learn More</Button>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default AllProjects;
